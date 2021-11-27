8ecf2d59d1fb6315c2bd9f872b5ff012bc974158
Aurora Grippaudo <aurora268@hotmail.it>
2021-11-27 15:55:01 +0100

    Adding cards

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    <img :src="image" :alt="title" />
+    <h3>{{title}}</h3>     
+  </div>
+</template>
+
+<script>
+export default {
+name:'Card',
+props:{
+    image: String,
+    price: String,
+    title:String,
+    type:String,
+},
+}
+</script>
+
+<style scoped lang="scss">
+div{
+  text-align:center;
+}
+h3{
+    color:white;
+}
+</style>
